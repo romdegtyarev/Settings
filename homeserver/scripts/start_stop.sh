bbd5061b7dc3e067611232531193f4ca4dc5700d
Roman Degtyarev <guromka@gmail.com>
2023-07-17 12:22:32 +0700

    Server update starter..

--- homeserver/scripts/start_stop.sh
+++ homeserver/scripts/start_stop.sh
@@ -13,7 +13,8 @@ if [[ $mode == "Start" ]]
 then
     #Before start
     echo "Start"
-    sudo mount /dev/sdb1 /samba/private
+    sudo mount /dev/sdc1 /samba/private
+    sudo mount /dev/sdb /samba/private/TV_NEW
     sudo systemctl restart minidlna.service
     timedatectl set-timezone Asia/Novosibirsk
     sudo ntpdate time.google.com
@@ -22,6 +23,7 @@ elif [[ $mode == "Stop" ]]
 then
     #Before stop
     echo "Stop"
+    sudo umount /samba/private/TV_NEW
     sudo umount /samba/private
 
 else
