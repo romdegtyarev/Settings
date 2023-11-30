66a2f81d1c25bb6035d9f360362ccf9065a17da7
roman.degtyarev <roman.degtyarev@eltex.loc>
2023-11-30 11:23:30 +0700

    Fix i3.

---
+++ common/scripts/lock.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#ff00c3fc'
+TEXT='#ffee00e6'
+WRONG='#ff0008f4'
+VERIFYING='#1eff00f4'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+-f                           \
+--radius 180                 \
+--ring-width 12              \
+--screen 1                   \
+--blur 9                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"    \
+--keylayout 2                \
+--verif-text="Making call to Mr. Rom"    \
+--wrong-text="Suck!"                     \
+--noinput-text="Type?"                   \
+--lock-text="Get Out From My PC"         \
+--lockfailed-text="Lock failed!"
